8542719b53777eead35888e9e14877cb0981db94
Yukimono <gr33nmous3@gmail.com>
2015-01-09 21:26:45 +0530

    Hide overlay in alt+tab

--- OverlayPlugin/Util.cs
+++ OverlayPlugin/Util.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace RainbowMage.OverlayPlugin
 {
@@ -36,6 +38,57 @@ namespace RainbowMage.OverlayPlugin
             }
 
             return false;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr", SetLastError = true)]
+        private static extern IntPtr SetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLong", SetLastError = true)]
+        private static extern Int32 SetWindowLong(IntPtr hWnd, int nIndex, Int32 dwNewLong);
+
+        [DllImport("kernel32.dll", EntryPoint = "SetLastError")]
+        public static extern void SetLastError(int dwErrorCode);
+        
+        private static int ptr32(IntPtr intPtr)
+        {
+            return unchecked((int)intPtr.ToInt64());
+        }
+
+        public static IntPtr SetWindowLongA(IntPtr hWnd, int nIndex, IntPtr dwNewLong)
+        {
+            int error = 0;
+            IntPtr result = IntPtr.Zero;
+
+            SetLastError(0);
+
+            if (IntPtr.Size == 4)
+            {
+                Int32 result32 = SetWindowLong(hWnd, nIndex, ptr32(dwNewLong));
+                error = Marshal.GetLastWin32Error();
+                result = new IntPtr(result32);
+            }
+            else
+            {
+                result = SetWindowLongPtr(hWnd, nIndex, dwNewLong);
+                error = Marshal.GetLastWin32Error();
+            }
+
+            if ((result == IntPtr.Zero) && (error != 0))
+            {
+                throw new System.ComponentModel.Win32Exception(error);
+            }
+
+            return result;
+        }
+
+        public static void Hide(System.Windows.Forms.Form form)
+        {
+            int ex = (int)GetWindowLong(form.Handle, (int)(-20));
+            ex |= (int)(0x00000080);
+            SetWindowLongA(form.Handle, (int)(-20), (IntPtr)ex);
         }
     }
 }
