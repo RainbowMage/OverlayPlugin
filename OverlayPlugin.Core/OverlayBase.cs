7853834b26055490105636c182539b5c7076b8e7
Tomonori Tamagawa <xtuaok@gmail.com>
2015-07-19 04:31:49 +0900

    Fix null object reference after dispose OverlayBase.

--- OverlayPlugin.Core/OverlayBase.cs
+++ OverlayPlugin.Core/OverlayBase.cs
@@ -294,6 +294,10 @@ namespace RainbowMage.OverlayPlugin
                 {
                     this.timer.Stop();
                 }
+                if (this.xivWindowTimer != null)
+                {
+                    this.xivWindowTimer.Stop();
+                }
                 if (this.Overlay != null)
                 {
                     this.Overlay.Close();
